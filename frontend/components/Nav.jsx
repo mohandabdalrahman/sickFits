b4425b6dcd4621d38c766a48f5aa1442f3e65667
Mohand Abdalrahman <mohand.abdalrahman@mondia.com>
2021-03-20 15:36:13 +0200

    finish count style

--- frontend/components/Nav.jsx
+++ frontend/components/Nav.jsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
 import NavStyles from '../components/styles/NavStyles'
 import { useCart } from '../lib/cartState'
+import CartCount from './CartCount'
 import SignOut from './SignOut'
 import { useUser } from './User'
 const Nav = () => {
   const user = useUser()
+  console.log('🚀 ~ file: Nav.jsx ~ line 9 ~ Nav ~ user', user)
+  const carts = []
+  carts.push(user?.cart)
   const { openCart } = useCart()
   return (
     <NavStyles>
@@ -15,7 +19,10 @@ const Nav = () => {
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
           <SignOut />
-          <button type="button" onClick={openCart}>My Cart</button>
+          <button type="button" onClick={openCart}>
+            My Cart
+            <CartCount count={carts.reduce((tally, cartItem) => tally + cartItem.quantity, 0)} />
+          </button>
         </>
       ) : <Link href="/signin">Sign In</Link>
       }
